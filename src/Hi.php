a87507f6199aa1973cc027d46306ec5966b0908a
kingkoer <2015933188@qq.com>
2021-11-05 13:38:42 +0800

    pack

---
+++ src/Hi.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace propack;
+
+class Hi {
+	public function xml($data = array()) {
+		header("Content-type:application/json");
+		echo '<?xml version="1.0" encoding="utf-8"?>';
+		echo '<data>';
+		foreach ($data as $k => $v) {
+			echo "<{$k}>$v</$k>";
+		}
+		echo '</data>';
+		exit;
+	}
+
+	public function json($data = array()) {
+		header("Content-type:application/json");
+		echo json_encode($data);
+		exit;
+	}
+
+}
